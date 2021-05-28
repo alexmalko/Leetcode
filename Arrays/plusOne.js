def0af39b7cdc30937ec5469022c386e8827a888
Alex Malko <alexmalko@live.ru>
2021-05-28 11:26:24 -0700

    plusOne.js changed

--- Arrays/plusOne.js
+++ Arrays/plusOne.js
@@ -2,7 +2,7 @@ var plusOne = function (digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] !== 9) {
       digits[i]++;
-      break;
+      return digits;
     } else {
       digits[i] = 0;
     }
