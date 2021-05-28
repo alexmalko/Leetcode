697e243398eef6cd5f5fc5de39ae4696f48b9e4c
Alex Malko <alexmalko@live.ru>
2021-05-28 16:04:12 -0700

    Product of Arrays except self solved

--- Arrays/ProductofArrayExceptSelf.js
+++ Arrays/ProductofArrayExceptSelf.js
@@ -1,8 +1,9 @@
+// solution without devistion
 var productExceptSelf = function (nums) {
-  let output = [],
-    left = [],
-    right = [];
-  let productFromLeft = 1,
+  let left = [],
+    right = [],
+    final = [],
+    productFromLeft = 1,
     productFromRight = 1;
   for (let i = 0; i < nums.length; i++) {
     left[i] = productFromLeft;
@@ -12,11 +13,24 @@ var productExceptSelf = function (nums) {
     right[i] = productFromRight;
     productFromRight *= nums[i];
   }
-
-  for (let i = 0; i < nums.length; i++) {
-    output[i] = left[i] * right[i];
+  for (let i = 0; i < left.length; i++) {
+    final.push(left[i] * right[i]);
   }
-  return left;
+
+  return final;
 };
 
 console.log(productExceptSelf([1, 2, 3, 4]));
+
+// solution with devisiton
+// var productExceptSelf = function (nums) {
+//   let product = 1;
+//   let arr = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     product = product * nums[i];
+//   }
+//   nums.forEach((e) => {
+//     arr.push(product / e);
+//   });
+//   return arr;
+// };
