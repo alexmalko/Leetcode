e1b73f9c0e99d8ed5721bc2fda01331a67b62567
Alex Malko <alexmalko29@gmail.com>
2021-06-02 07:01:20 -0700

    validpalindrome solved

--- Arrays/Permutations.js
+++ Arrays/Permutations.js
@@ -1,26 +1,42 @@
-var permute = function (nums) {
-  let res = [],
-    used = new Array(nums.length).fill(false);
-  function permuteHelper(depth, used, cur) {
-    if (depth == nums.length) {
-      res.push(cur.slice());
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (used[i] == false) {
-        // nums[i] is already being used
-        cur.push(nums[i]);
-        used[i] = true;
+// var permute = function (nums) {
+//   let res = [],
+//     used = new Array(nums.length).fill(false);
+//   console.log(used);
+//   function permuteHelper(depth, used, cur) {
+//     if (depth == nums.length) {
+//       res.push(cur.slice());
+//       return;
+//     }
+//     for (let i = 0; i < nums.length; i++) {
+//       if (used[i] == false) {
+//         // nums[i] is already being used
+//         cur.push(nums[i]);
+//         used[i] = true;
 
-        // move to the next depth
-        permuteHelper(depth + 1, used, cur);
+//         // move to the next depth
+//         permuteHelper(depth + 1, used, cur);
 
-        // backtrack to previous state
-        cur.pop();
-        used[i] = false; // nums[i] is no more being used
-      }
-    }
+//         // backtrack to previous state
+//         cur.pop();
+//         used[i] = false; // nums[i] is no more being used
+//       }
+//     }
+//   }
+//   permuteHelper(0, used, []);
+//   return res;
+// };
+
+var permute = function (nums, set = [], answer = []) {
+  if (!nums.length) {
+    answer.push([...set]);
   }
-  permuteHelper(0, used, []);
-  return res;
+  for (let i = 0; i < nums.length; i++) {
+    const newNums = nums.filter((n, index) => index !== i);
+    set.push(nums[i]);
+    permute(newNums, set, answer);
+    set.pop();
+  }
+  return answer;
 };
+
+console.log(permute([1, 2, 3]));
--- Arrays/ValidPalindrome.js
+++ Arrays/ValidPalindrome.js
@@ -1,3 +1,24 @@
-var isPalindrome = function(s) {
-    
+var isPalindrome = function (s) {
+  // convert a string to a lower case and remove all of the special characters using regex
+  s = s
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-zA-Z0-9]/g, "");
+  console.log(s);
+  let beg = 0,
+    end = s.length - 1;
+  while (beg < end) {
+    if (s[beg] !== s[end]) {
+      return false;
+    }
+    beg++;
+    end--;
+  }
+
+  //   for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+  //     if (s[i] !== s[j]) return false;
+  //   }
+  return true;
 };
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
