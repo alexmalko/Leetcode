eda1994e04da7f9fe4793df192bc8d85a124d7e2
Alex Malko <alexmalko@live.ru>
2021-06-01 20:26:52 -0700

    pascal's triangle

--- Arrays/PascalsTriangle.js
+++ Arrays/PascalsTriangle.js
@@ -1,15 +1,14 @@
 var generate = function (numRows) {
-  let result = [];
-  result.push([1]);
+  let result = [[1]];
   for (let i = 1; i < numRows; i++) {
     let prev = result[i - 1];
-    let current = [];
-    current.push(1);
+    let curr = [];
+    curr.push(1);
     for (let j = 1; j < prev.length; j++) {
-      current.push(prev[j - 1] + prev[j]);
+      curr.push(prev[j - 1] + prev[j]);
     }
-    current.push(1);
-    result.push(current);
+    curr.push(1);
+    result.push(curr);
   }
   return result;
 };
