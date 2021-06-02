5215012fe66250cb31ad050180d57e01b6e9738f
Alex Malko <alexmalko@live.ru>
2021-06-02 09:49:16 -0700

    ExcelSheetColumnNumber.js solved

--- Arrays/ExcelSheetColumnNumber.js
+++ Arrays/ExcelSheetColumnNumber.js
@@ -1,1 +1,12 @@
-var titleToNumber = function (s) {};
+var titleToNumber = function (columnTitle) {
+  let sum = 0,
+    exp = 0;
+  for (let i = columnTitle.length - 1; i >= 0; i--) {
+    sum += (columnTitle[i].charCodeAt() % 64) * 26 ** exp;
+    exp++;
+  }
+
+  return sum;
+};
+
+console.log(titleToNumber("ABB"));
