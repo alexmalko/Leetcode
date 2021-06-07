47a2801d02328ea562b0071828617e898ea123c1
Alex Malko <alexmalko@live.ru>
2021-06-06 18:06:46 -0700

    best time to buy and sell stock.js

--- Arrays/BestTimetoBuyandSellStock.js
+++ Arrays/BestTimetoBuyandSellStock.js
@@ -1,14 +1,14 @@
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+  let minPrice = Infinity,
+    profit = 0;
+  prices.forEach((e) => {
+    if (minPrice > e) {
+      minPrice = e;
+    } else if (profit < e - minPrice) {
+      profit = e - minPrice;
+    }
+  });
+  return profit;
+};
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-
-// let minPrice = Infinity,
-//   profit = 0;
-// for (const price of prices) {
-//   if (price < minPrice) {
-//     minPrice = price;
-//   } else if (price - minPrice > profit) {
-//     profit = price - minPrice;
-//   }
-// }
-// return profit;
