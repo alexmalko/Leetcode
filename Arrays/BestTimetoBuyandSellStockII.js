d95ff348cad774b523efa7430b28dc072450b933
Alex Malko <alexmalko@live.ru>
2021-06-06 19:00:52 -0700

    happy number.js solved

--- Arrays/BestTimetoBuyandSellStockII.js
+++ Arrays/BestTimetoBuyandSellStockII.js
@@ -9,15 +9,3 @@ var maxProfit = function (prices) {
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
-
-// var maxProfit = function (prices) {
-//   let total = 0;
-//   for (let i = 0; i < prices.length; i++) {
-//     if (prices[i] < prices[i + 1]) {
-//       total += prices[i + 1] - prices[i];
-//     } else {
-//       continue;
-//     }
-//   }
-//   return total;
-// };
--- Arrays/ClimbingStairs.js
+++ Arrays/ClimbingStairs.js
@@ -14,33 +14,4 @@ function climbStairs(n) {
   return helper(n);
 }
 
-// // no memoization
-// var climbStairs = function (n) {
-//   let cache = {};
-//   if (n == 0 || n == 1) {
-//     return 1;
-//   }
-//   if (!cache[n]) {
-//     cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
-//   }
-//   return cache[n];
-// };
-
-// // no memoization
-// var climbStairs = function (n) {
-//   if (n == 0 || n == 1) {
-//     return 1;
-//   }
-//   return climbStairs(n - 1) + climbStairs(n - 2);
-// };
-
-// // iterative
-// function climbStairs(n) {
-//   let result = [1, 1];
-//   for (let i = 2; i <= n; i++) {
-//     result.push(result[i - 2] + result[i - 1]);
-//   }
-//   return result[n];
-// }
-
 console.log(climbStairs(4));
